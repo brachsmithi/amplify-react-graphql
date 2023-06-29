405f1c84937414ecd803c0a2581305cb3547586b
brachsmithi <bsmithi.impaired@gmail.com>
2023-06-29 16:04:20 -0400

    Trying to make amplify take my change

--- src/App.js
+++ src/App.js
@@ -58,10 +58,10 @@ const App = ({ signOut }) => {
     event.target.reset();
   }
 
-  async function deleteNote({ id }) {
+  async function deleteNote({ id, name }) {
     const newNotes = notes.filter((note) => note.id !== id);
     setNotes(newNotes);
-    await Storage.remove(note);
+    await Storage.remove(name);
     await API.graphql({
       query: deleteNoteMutation,
       variables: { input: { id } },
